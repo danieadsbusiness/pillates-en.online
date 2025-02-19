c30e34acab13bc07c657e5eefdf974031c682871
danieadsbusiness <daniel.ads.business@outlook.com>
2025-02-19 15:44:15 -0300

    Add files via upload

---
+++ QPI/js/config.js
@@ -0,0 +1,24 @@
+window.config = {
+  PARTNER_ID: "018cb48c-a52f-e813-aea0-dd8414bff280",
+  PAGE_SET_ID: "018ecd5e-8c88-9315-37c9-ac5e7a993931",
+  APP_ID: "018cb4b8-885c-523d-d99d-3976b0b817c0",
+  APP_GOOGLE_LINK: "https://play.google.com/store/apps/details?id=com.organicapps.fit",
+  APP_APPLE_LINK: "https://apps.apple.com/us/app/organic-fit-home-weight-loss/id1188810254",
+  GOOGLE_CUSTOMER_ID: "None",
+  PIXEL_ID: "1243909586171637",
+
+  API_SERVER: "https://bgv-backend.getbraavo.com",
+  API_VERSION: "v1",
+  EVENTS_API_SERVER: "https://bgv-event-ingestor.getbraavo.com",
+  EVENTS_API_VERSION: "v1",
+  EXPERIMENT_NAME: "of-wp-03",
+  STRIPE_PUBLISHED_KEY: "pk_live_51NDTUgGVbYMOy0SeTibIeQmMHEcbyr0ThHQ260aBZckNv1s2r06rMs4rOVgoKFmcZPG6scO0pfAUgdBBQMRQvK6300Vs3fZyS2",
+  PAY_PAL_CLIENT_ID: "Ae_SneYuS9SEpu_DXcFdO2dJITwPqGHewdO36QbN_FH6OBIdzUbdJGIQKzdYn6x4_PG3QXz7arEF8_IB",
+
+  
+  
+  PAYMENT_SCREEN_VERSION: "default",
+
+  PAYMENT_METHODS: ['stripe', 'paypal'],
+  PLANS: [{"name":"1-month plan","planInternalId":"018d6460-5825-5e40-8ecd-7d4fd1032636","paypalPlanId":"P-7JU59722MY039090JMVY7UTA","interval":"month","intervalCount":1,"currency":"USD","basePriceAmount":"19.99","accentPrimary":"most popular","preselected":true,"order":1,"offerType":"b","periodPrice":"4.99","periodLabel":"per week","crossedPrice":"32.99","crossedPeriodPrice":"8.24","discountPercentage":"40%","totalPeriodLabel":"per 1 month"},{"name":"6-month plan","planInternalId":"018d6462-8e57-bdb0-b75d-c926ce1ccddb","paypalPlanId":"P-55T62816HV661074XMVY7WNA","interval":"month","intervalCount":6,"currency":"USD","basePriceAmount":"39.99","preselected":true,"order":2,"offerType":"b","periodPrice":"1.69","periodLabel":"per week","crossedPrice":"113.99","crossedPeriodPrice":"","discountPercentage":"65%"},{"name":"3-month plan","planInternalId":"018d6461-7799-abf0-676a-e7de96bbd06d","paypalPlanId":"P-67V93862E71272010MVY7VTA","interval":"month","intervalCount":3,"currency":"USD","basePriceAmount":"29.99","preselected":false,"order":0,"offerType":"b","periodPrice":"2.39","periodLabel":"per week","crossedPrice":"59.99","crossedPeriodPrice":"","discountPercentage":"50%"}]
+};
